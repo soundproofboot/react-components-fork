8cd16845bdc4c60335f66c750fd68b0203001ab0
DYZ-1234 <drewweise@gmail.com>
2026-04-08 12:34:57 -0500

    Seeing bootstrap work in our app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = () => {
 
   return (
-    <>
+    <div className="m-3">
       <h1>
         React Components
       </h1>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
