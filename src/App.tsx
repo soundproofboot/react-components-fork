3504e6f8a7c785dbfd22c3233069ab380024a102
lmeier127 <Lmeier127@gmail.com>
2026-04-12 11:54:04 -0500

    Pizza toppings moved to its own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PizzaToppings from './PizzaToppings.tsx';
 
-const PizzaToppings = () => { 
-    return (
-        <h2>
-            Choose your pizza toppings...
-        </h2>
-    );
-}
 
 const App = () => {
 
---
+++ src/PizzaToppings.tsx
@@ -0,0 +1,9 @@
+const PizzaToppings = () => { 
+    return (
+        <h2>
+            Choose your pizza toppings...
+        </h2>
+    );
+}
+
+export default PizzaToppings;
