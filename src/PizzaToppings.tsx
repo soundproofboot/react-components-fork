3337fe4bc9bfeb63f738ae59be18f578e28e5782
lmeier127 <Lmeier127@gmail.com>
2026-04-12 12:22:39 -0500

    all/none working and total calculated with map filter reduce

--- src/PizzaToppings.tsx
+++ src/PizzaToppings.tsx
@@ -19,7 +19,14 @@ const [availablePizzaToppings, setAvailablePizzaToppings] = useState(
 //
 //Calculate derived state and other code
 //
-
+const total= availablePizzaToppings
+    .filter(
+        x => x.checked,
+    )
+    .reduce(
+        (acc, x) => acc + x.price,
+        0,
+    )
 //
 //Return JSX...
 //
@@ -33,12 +40,32 @@ const [availablePizzaToppings, setAvailablePizzaToppings] = useState(
                 </Card.Title>
                 <Button
                     variant="outline-primary"
+                    onClick={
+                        () => setAvailablePizzaToppings(
+                            availablePizzaToppings.map(
+                                x => ({
+                                    ...x, 
+                                    checked: true
+                                })
+                            )
+                        )
+                    }
                 >
                     All
                 </Button>
                 <Button
                 className="ms-2"
                     variant="outline-primary"
+                    onClick={
+                        () => setAvailablePizzaToppings(
+                            availablePizzaToppings.map(
+                                x => ({
+                                    ...x, 
+                                    checked: false
+                                })
+                            )
+                        )
+                    }
                 >
                     None
                 </Button>
@@ -49,7 +76,7 @@ const [availablePizzaToppings, setAvailablePizzaToppings] = useState(
                                 className="my-2"
                             >
                                 {
-                                    `${x.name} (${x.price})`
+                                    `${x.name} ($${x.price.toFixed(2)})`
                                 }
                             </div>
                         )
@@ -57,7 +84,9 @@ const [availablePizzaToppings, setAvailablePizzaToppings] = useState(
                 }
                 
                 <h3>
-                    Total: 0.00
+                    Total: {
+                        `$${total.toFixed(2)}`
+                    }
                 </h3>
             </Card.Body>
         </Card>
