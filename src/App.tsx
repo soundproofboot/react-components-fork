334338617486705b9627a7e523e0d95a3fd0c00b
THamilton1123 <thamilton12@madisoncollege.edu>
2026-04-13 15:25:00 -0500

    Created pizza toppings component...
    Next, we'll move it to its own file...

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+/*
+The below line of code is the better way to write it since the function has only the one, small thing it's returning.
+const PizzaToppings = () => <h2>Choose your pizza toppings...</h2>;
+*/
+// But this way (notice the curly brackets, and return keyword) would allow for more lines of code to be executed upon calling the PizzaToppings function
+const PizzaToppings = () => {
+  return <h2>Choose your pizza toppings...</h2>;
+};
 const App = () => {
   return (
     <div className="m-3">
       <h1>React Components</h1>
+      <PizzaToppings />
     </div>
   );
 };
